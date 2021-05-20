749081a2fb3bb61ec452941dffccb5f807dd2d90
thinga <32902065+thinga@users.noreply.github.com>
2021-05-20 14:43:39 +0200

    68. Updating Posts on the Server,Backend Routes

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -23,12 +23,15 @@ export class PostCreateComponent implements OnInit {
 
   ngOnInit() {
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has('postId')) {
-        this.mode = 'edit';
-        this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+       
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
       } else {
-        this.mode = 'create';
+        this.mode = "create";
         this.postId = null;
       }
     });
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -39,8 +39,11 @@ export class PostsService {
     }
 
     getPost(id: string) {
-        return {...this.posts.find(p => p.id === id)};
+        return this.http.get<{_id: string; title: string; content: string}>(
+            "http://localhost:3000/api/posts/" + id
+        );
     }
+
     
     addPost(title: string, content: string) {
         const post: Post = {id: null, title: title, content: content};
@@ -58,7 +61,13 @@ export class PostsService {
         const post: Post = { id: id, title: title, content: content };
         this.http
           .put("http://localhost:3000/api/posts/" + id, post)
-          .subscribe(response => console.log(response));
+          .subscribe(response => {
+              const updatedPosts = [...this.posts];
+              const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+              updatedPosts[oldPostIndex] = post;
+              this.posts = updatedPosts;
+              this.postsUpdated.next([...this.posts]);
+          });
       }
 
 
