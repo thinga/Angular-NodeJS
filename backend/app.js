c6ec222f0b5dc905ae43444839e41cea0612d2f6
thinga <32902065+thinga@users.noreply.github.com>
2021-05-25 16:28:34 +0200

    83. Fetching Images on the Frontend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -17,6 +18,7 @@ mongoose.connect("mongodb+srv://nga:dlQIa3agpH4dXSvr@cluster0.kpj56.mongodb.net/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
