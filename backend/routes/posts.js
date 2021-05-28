21f9d653372e816636ace52fcee79ad8f94886a8
thinga <32902065+thinga@users.noreply.github.com>
2021-05-28 16:34:52 +0200

    104.Adding Middleware to Project Routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const multer = require("multer");
-const { count } = require("../models/post");
+
 const Post = require('../models/post');
+const checkAuth = require("../middleware/check-auth");
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -98,7 +99,7 @@ router.get("/:id", (req, res, next) => {
     });
 
 
-    router.delete("/:id", (req, res, next) => {
+    router.delete("/:id", checkAuth, (req, res, next) => {
     Post.deleteOne({ _id: req.params.id }).then(result => {
         console.log(result);
         res.status(200).json({ message: "Post deleted!" });
