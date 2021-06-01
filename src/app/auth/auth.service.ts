27d337b48950a972e34e219053f62b47fe8dcd64
thinga <32902065+thinga@users.noreply.github.com>
2021-06-01 23:31:31 +0200

    109. Redirecting Users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { AuthData } from "./auth-data.model";
@@ -9,7 +10,7 @@ export class AuthService {
    private isAuthenticated = false;
    private token: string;
    private authStatusListerner = new Subject<boolean>();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getToken() {
     return this.token;
@@ -40,6 +41,7 @@ export class AuthService {
       if (token) {
         this.isAuthenticated = true;
         this.authStatusListerner.next(true);
+        this.router.navigate(['/']);
       }
      
     });
