40c17d4f245edf0304596c9bd2e63657f3ba25e0
thinga <32902065+thinga@users.noreply.github.com>
2021-06-14 22:04:13 +0200

    111. Reflecting the Token Expiration in the UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { AuthData } from "./auth-data.model";
 export class AuthService {
   private isAuthenticated = false;
    private token: string;
+   private tokenTimer: any;
    private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) { }
@@ -34,11 +35,16 @@ export class AuthService {
    
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http
+    .post<{ token: string, expiresIn: number }>("http://localhost:3000/api/user/login", authData)
     .subscribe(response => {
       const token = response.token;
       this.token = token;
       if (token) {
+        const expiresInDuration = response.expiresIn;
+        this.tokenTimer = setTimeout(() => {
+           this.logout();
+        }, expiresInDuration * 1000);
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         this.router.navigate(["/"]);
@@ -52,6 +58,8 @@ export class AuthService {
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
     this.router.navigate(["/"]);
+    clearTimeout(this.tokenTimer);
+    this.router.navigate(['/']);
   }
   
   
